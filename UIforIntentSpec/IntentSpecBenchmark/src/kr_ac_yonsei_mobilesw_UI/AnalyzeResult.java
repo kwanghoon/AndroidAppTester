7c4736af9928018f915fa59041918bee1a0fd457
Kwanghoon Choi <lazyswamp@gmail.com>
2015-06-06 10:41:09 +0900

    delete UIforIntentSpec/IntentSpecBenchmark/*

--- UIforIntentSpec/IntentSpecBenchmark/src/kr_ac_yonsei_mobilesw_UI/AnalyzeResult.java
+++
@@ -1,8 +0,0 @@
-package kr_ac_yonsei_mobilesw_UI;
-
-public enum AnalyzeResult {
-	Normal, Exit, ErrorExit, 
-	IntentSpecCatchAndNormal, IntentSpecCatchAndExit, IntentSpecCatchAndErrorExit,
-	IntentSpecPassAndNormal, IntentSpecPassAndExit, IntentSpecPassAndErrorExit,
-	CantAnalyze;
-}
--- UIforIntentSpec/IntentSpecBenchmark/src/kr_ac_yonsei_mobilesw_UI/ComponentMode.java
+++
@@ -1,5 +0,0 @@
-package kr_ac_yonsei_mobilesw_UI;
-
-public enum ComponentMode {
-	Activity, BroadcastReceiver, Service;
-}
